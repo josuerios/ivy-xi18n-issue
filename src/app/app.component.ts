ecd4555560c020a8ffa769ed53947dc491d014d4
Josue Rios Diaz <josue.riosdiaz@almundo.com>
2019-12-05 16:37:55 -0300

    feat: add localized code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'ivy-i18n';
+  public message: string;
+
+  ngOnInit(): void {
+    this.message = $localize`:@@anId:a basic message`;
+  }
 }
